ecbb7562a2940869ff55eb30a87f23f6a9e95710
马晨杰 <309272985@qq.com>
2023-01-11 12:10:00 +0800

    feat: deepclone pro

--- packages/makeWheels/deepClone.ts
+++ packages/makeWheels/deepClone.ts
@@ -1,30 +1,47 @@
-
-import {cloneDeep} from "lodash"
-const deepClone=(original,cache = new WeakMap())=>{
-  let cloneObject;
-  if(typeof original === "object"){
-    cloneObject = Array.isArray(original) ? [] : {};
-    if(cache.has(original)){
-      return cache.get(original)
+import { cloneDeep, isSet } from "lodash";
+const deepClone = (original, cache = new WeakMap()) => {
+  if (typeof original === "object") {
+    let cloneObject = Array.isArray(original) ? [] : {};
+    if (cache.has(original)) {
+      return cache.get(original);
     }
-    cache.set(original,cloneObject)
-    for(let key in original){
-      const clone = deepClone(original[key],cache);
+    cache.set(original, cloneObject);
+
+    if (original instanceof Set) {
+      cloneObject = new Set();
+      original.forEach((value) => {
+        const clone = deepClone(value, cache);
+        cloneObject.add(clone);
+      });
+      return cloneObject;
+    }
+
+    if (original instanceof Map) {
+      cloneObject = new Map();
+      original.forEach((value, key) => {
+        const clone = deepClone(value, cache);
+        cloneObject.set(key, clone);
+      });
+      return cloneObject;
+    }
+
+    for (let key in original) {
+      const clone = deepClone(original[key], cache);
       cloneObject[key] = clone;
     }
-  }else{
-    cloneObject=original
+    return cloneObject;
+  } else {
+    return original;
   }
-  return cloneObject
-}
-
+};
 
 let a = {
-  name:1,
+  dd: Symbol(213),
+  name: new Set([12, 22]),
 };
 
 a.ccc = a;
 
 const d = deepClone(a);
 
-console.log(d)
+console.log(d);
