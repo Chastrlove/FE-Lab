7c615318c1efb5de9a16e94a2373d0eef5a6d2d4
马晨杰 <309272985@qq.com>
2023-01-23 15:39:16 +0800

    feat:二叉树

--- packages/algorithm/base.ts
+++ packages/algorithm/base.ts
@@ -7,9 +7,9 @@ export class ListNode {
   }
 }
 
-export const createLinkedHead = (input: Array<number>) => {
-  let head = null;
-  let ans = null;
+export const createLinkedHead = (input: Array<number>): ListNode => {
+  let head = null as unknown as ListNode;
+  let ans = null as unknown as ListNode;
   for (let c of input) {
     const node = new ListNode(c);
     if (head) {
@@ -22,3 +22,46 @@ export const createLinkedHead = (input: Array<number>) => {
   }
   return head;
 };
+
+export class TreeNode {
+  public value: number;
+  constructor(value) {
+    this.value = value;
+  }
+  public left?: TreeNode;
+  public right?: TreeNode;
+}
+//   1
+// 2   3
+//4 5 6 7
+export const createTreeNode = () => {
+  const root = new TreeNode(1);
+  root.left = new TreeNode(2);
+  root.right = new TreeNode(3);
+  root.left.left = new TreeNode(4);
+  root.left.right = new TreeNode(5);
+  root.right.left = new TreeNode(6);
+  root.right.right = new TreeNode(7);
+  return root;
+};
+
+export function levelOrderCreateTree(arr) {
+  if (arr.length === 0) return null;
+  let root = new TreeNode(arr[0]);
+  let queue = [root];
+  let i = 1;
+  while (queue.length > 0 && i < arr.length) {
+    let current = queue.shift();
+    let left = arr[i] !== null ? new TreeNode(arr[i]) : null;
+    let right = arr[i + 1] !== null ? new TreeNode(arr[i + 1]) : null;
+    current.left = left;
+    current.right = right;
+    if (left !== null) queue.push(left);
+    if (right !== null) queue.push(right);
+    i += 2;
+  }
+  return root;
+}
+
+
+// console.log(levelOrderCreateTree([3,5,1,6,2,0,8,null,null,7,4]))
