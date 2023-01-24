0ef24d329dae8c2c5b044785fb64762ddc0caa0f
马晨杰 <309272985@qq.com>
2023-01-24 22:38:00 +0800

    feat:桶排序

--- packages/algorithm/heap/前K个高频元素.ts
+++ packages/algorithm/heap/前K个高频元素.ts
@@ -57,6 +57,43 @@ let minHeapify = (array, map, heapSize, k) => {
   }
 };
 
-const a = topKFrequent([1,1,1,2,2,3], 2);
+let topKFrequentNew = function (nums, k) {
+  let map = new Map(),
+    arr = [...new Set(nums)];
+  nums.map((num) => {
+    if (map.has(num)) map.set(num, map.get(num) + 1);
+    else map.set(num, 1);
+  });
 
-console.log(a)
+  // 如果元素数量小于等于 k
+  if (map.size <= k) {
+    return [...map.keys()];
+  }
+
+  return bucketSort(map, k);
+};
+
+// 桶排序
+let bucketSort = (map, k) => {
+  let arr: Array<Array<number>> = [];
+  let res: Array<number> = [];
+  map.forEach((value, key) => {
+    // 利用映射关系（出现频率作为下标）将数据分配到各个桶中
+    if (!arr[value]) {
+      arr[value] = [key];
+    } else {
+      arr[value].push(key);
+    }
+  });
+  // 倒序遍历获取出现频率最大的前k个数
+  for (let i = arr.length - 1; i >= 0 && res.length < k; i--) {
+    if (arr[i]) {
+      res.push(...arr[i]);
+    }
+  }
+  return res;
+};
+
+const a = topKFrequentNew([1, 1, 1, 1, 2, 2, 3], 2);
+
+console.log(a);
